54bbf474f59ded7b5599e92a4369713aee88bbca
Binalee <Binalee@DESKTOP-64MV3HQ>
2021-07-09 20:10:13 +0530

    New Testcase 'second'

---
+++ Test Suites/TS_first.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_first</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>afce2af6-68be-49cd-a7fe-e7be2c139671</testSuiteGuid>
+   <testCaseLink>
+      <guid>b393e558-1109-4bd1-b22a-3e735e8ddd78</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/first</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
